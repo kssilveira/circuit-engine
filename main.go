a9a32dfb4659f9439314782ca6c5d51c0562eb5f
Kaue Silveira <silveira.kaue@gmail.com>
2025-11-28 17:20:29 +0100

    main refactor

--- main.go
+++ main.go
@@ -11,7 +11,27 @@ import (
 	"github.com/kssilveira/circuit-engine/lib"
 )
 
+func main() {
+	if err := all(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func all() error {
+	exampleName := flag.String("example_name", "TransistorEmitter", "example name")
+	c := circuit.NewCircuit(flagsToConfig())
+	outs := lib.Example(c, *exampleName)
+	if len(outs) == 0 {
+		return fmt.Errorf("invalid --example_name %q, valid names are %q", *exampleName, lib.ExampleNames())
+	}
+	c.Outs(outs)
+	res := c.Simulate()
+	fmt.Println(strings.Join(res, "\n\n"))
+	return draw(res, c.Config)
+}
+
+func flagsToConfig() config.Config {
 	maxPrintDepth := flag.Int("max_print_depth", -1, "max print depth")
 	drawGraph := flag.Bool("draw_graph", false, "draw graph")
 	drawSingleGraph := flag.Bool("draw_single_graph", false, "draw single graph")
@@ -19,11 +39,8 @@ func all() error {
 	drawEdges := flag.Bool("draw_edges", true, "draw edges")
 	drawShapePoint := flag.Bool("draw_shape_point", false, "draw shape point")
 	isUnitTest := flag.Bool("is_unit_test", false, "is unit test")
-	exampleName := flag.String("example_name", "TransistorEmitter", "example name")
-
 	flag.Parse()
-
-	c := circuit.NewCircuit(config.Config{
+	return config.Config{
 		MaxPrintDepth:   *maxPrintDepth,
 		DrawGraph:       *drawGraph,
 		DrawSingleGraph: *drawSingleGraph,
@@ -31,22 +48,15 @@ func all() error {
 		DrawEdges:       *drawEdges,
 		DrawShapePoint:  *drawShapePoint,
 		IsUnitTest:      *isUnitTest,
-	})
-
-	outs := lib.Example(c, *exampleName)
-	if len(outs) == 0 {
-		return fmt.Errorf("invalid --example_name %q, valid names are %q", *exampleName, lib.ExampleNames())
 	}
-	c.Outs(outs)
+}
 
-	res := c.Simulate()
-	fmt.Println(strings.Join(res, "\n\n"))
-
-	if !*drawGraph {
+func draw(res []string, cfg config.Config) error {
+	if !cfg.DrawGraph {
 		return nil
 	}
 	for i, graph := range res {
-		if i >= 4 || (*drawSingleGraph && i >= 1) {
+		if i >= 4 || (cfg.DrawSingleGraph && i >= 1) {
 			break
 		}
 		if err := os.WriteFile(fmt.Sprintf("%d.dot", i), []byte(graph), 0644); err != nil {
@@ -54,11 +64,4 @@ func all() error {
 		}
 	}
 	return nil
-}
-
-func main() {
-	if err := all(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
 }
